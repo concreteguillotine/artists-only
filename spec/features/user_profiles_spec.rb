6659a036d248b670be8844ef58cc721a5d2148c9
concreteguillotine <sethlinex@gmail.com>
2022-08-31 20:24:36 -0400

    small clean up of test code

--- spec/features/user_profiles_spec.rb
+++ spec/features/user_profiles_spec.rb
@@ -1,28 +1,40 @@
 require "rails_helper"
 
 RSpec.feature "Users have profiles" do
-    let!(:user) { FactoryBot.create(:user) }
-
+    let!(:user1) { FactoryBot.create(:user, username: "user1") }
+    let!(:user2) { FactoryBot.create(:user, username: "user2") }
     before do
-        login_as(user)
+        login_as(user1)
         visit "/"
         click_link "New post"
 
         fill_in "Title", with: "Cool Project"
-        attach_file("Image", "spec/fixtures/testimage.jpg")
-        fill_in "Text", with: "This is my new project, add what you like!"
-        page.check("Open?")
         click_button "Create Post"
+
+        visit "/"
+        click_link "Sign out"
+
+        login_as(user2)
+        visit "/"
+        click_link "New post"
+
+        fill_in "Title", with: "Different Project"
+        click_button "Create Post"
+
+        click_link "Your profile"
     end
 
     scenario "they can visit their profile and see their own posts" do
-
-        click_link "Your profile"
-
         expect(page).to have_content "This is your profile"
 
         within(".posts") do
-            expect(page).to have_content "Cool Project"
+            expect(page).to have_content "Different Project"
+        end
+    end
+
+    scenario "but not other users' posts" do
+        within(".posts") do
+            expect(page).not_to have_content "Cool Project"
         end
     end
 end
